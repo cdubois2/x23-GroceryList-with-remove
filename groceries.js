e5686678cef978be632ea11a882be1a93831f741
Chris-PC\Chris <cdubois2@mail.csuchico.edu>
2016-11-14 17:24:15 -0800

    no message

---
+++ groceries.js
@@ -0,0 +1,48 @@
+function removeParentListItem() {
+    var mom = this.parentNode;
+    var grandma = mom.parentNode;
+    grandma.removeChild(mom);
+}
+
+function addItem(){
+    var input = document.getElementById("newItem").value;  
+    var item = document.createElement("li"); 
+    var itemName = document.createTextNode(input);
+    var btnClose = document.createElement("button");
+    var iconClose = document.createElement("span");
+    btnClose.classList.add("btn");
+    btnClose.classList.add("btn-danger");
+    btnClose.classList.add("btn-xs");
+    iconClose.classList.add("glyphicon");
+    iconClose.classList.add("glyphicon-remove");
+    btnClose.addEventListener("click", removeParentListItem);
+
+    item.appendChild(itemName);
+    btnClose.appendChild(iconClose);
+    item.appendChild(btnClose);
+    document.getElementById("listDisplay").appendChild(item);
+    document.getElementById("newItem").value = " ";
+}
+
+//courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+//courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
